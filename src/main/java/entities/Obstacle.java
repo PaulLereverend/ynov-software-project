46614efb9325cf2ee8280d94fe33415291a04921
PaulLereverend <nte.remix@gmail.com>
2019-11-05 11:18:18 +0100

    Add hibernate

--- src/main/java/entities/Obstacle.java
+++ src/main/java/entities/Obstacle.java
@@ -1,59 +1,110 @@
 package entities;
-
-
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
- 
+
+
 @Entity
-@Table(name = "obstacle")
 public class Obstacle {
- 
-    @Id
-    @Column(name = "user_id")
-    private int userid;
- 
-    @Column(name = "user_name")
-    private String username;
- 
-    @Column(name = "created_by")
-    private String createdBy;
- 
-    @Column(name = "created_date")
-    private Date createdDate;
- 
-    public int getUserid() {
-        return userid;
-    }
- 
-    public void setUserid(int userid) {
-        this.userid = userid;
-    }
- 
-    public String getUsername() {
-        return username;
-    }
- 
-    public void setUsername(String username) {
-        this.username = username;
-    }
- 
-    public String getCreatedBy() {
-        return createdBy;
-    }
- 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
- 
-    public Date getCreatedDate() {
-        return createdDate;
-    }
- 
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name = "traversable")
+	private int traversable;
+	
+	@Column(name = "effet")
+	private int effet;
+	
+	@Column(name = "nom")
+	private String nom;
+	
+	@Column(name = "apparence")
+	private String apparence;	
+	
+	@Column(name = "nbMaxParNiveau")
+	private int nbMaxParNiveau;
+	
+	@Column(name = "nbMinParNiveau")
+	private int nbMinParNiveau;
+	
+	public Obstacle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Obstacle(int id, int traversable, int effet, String nom, String apparence, int nbMaxParNiveau,
+			int nbMinParNiveau) {
+		super();
+		this.id = id;
+		this.traversable = traversable;
+		this.effet = effet;
+		this.nom = nom;
+		this.apparence = apparence;
+		this.nbMaxParNiveau = nbMaxParNiveau;
+		this.nbMinParNiveau = nbMinParNiveau;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTraversable() {
+		return traversable;
+	}
+
+	public void setTraversable(int traversable) {
+		this.traversable = traversable;
+	}
+
+	public int getEffet() {
+		return effet;
+	}
+
+	public void setEffet(int effet) {
+		this.effet = effet;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getApparence() {
+		return apparence;
+	}
+
+	public void setApparence(String apparence) {
+		this.apparence = apparence;
+	}
+
+	public int getNbMaxParNiveau() {
+		return nbMaxParNiveau;
+	}
+
+	public void setNbMaxParNiveau(int nbMaxParNiveau) {
+		this.nbMaxParNiveau = nbMaxParNiveau;
+	}
+
+	public int getNbMinParNiveau() {
+		return nbMinParNiveau;
+	}
+
+	public void setNbMinParNiveau(int nbMinParNiveau) {
+		this.nbMinParNiveau = nbMinParNiveau;
+	}
+	
+		
+	
+	
 }
