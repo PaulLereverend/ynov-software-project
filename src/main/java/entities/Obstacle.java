185dea8415269744835f0d47df245ff739924b61
PaulLereverend <paulereverend@gmail.com>
2019-11-05 10:55:26 +0100

    Add spring dependencies

---
+++ src/main/java/entities/Obstacle.java
@@ -0,0 +1,59 @@
+package entities;
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+ 
+@Entity
+@Table(name = "obstacle")
+public class Obstacle {
+ 
+    @Id
+    @Column(name = "user_id")
+    private int userid;
+ 
+    @Column(name = "user_name")
+    private String username;
+ 
+    @Column(name = "created_by")
+    private String createdBy;
+ 
+    @Column(name = "created_date")
+    private Date createdDate;
+ 
+    public int getUserid() {
+        return userid;
+    }
+ 
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getCreatedBy() {
+        return createdBy;
+    }
+ 
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+ 
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+ 
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
