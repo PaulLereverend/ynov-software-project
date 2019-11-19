61229e6af73fbb5c9e16c837fc990add992ed63d
PaulLereverend <nte.remix@gmail.com>
2019-11-19 09:28:32 +0100

    Add entities

---
+++ src/main/java/entities/Historique.java
@@ -0,0 +1,28 @@
+package entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Historique {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	
+	@ManyToOne
+    @JoinColumn
+    private Niveau niveau;
+	
+	@Column(name = "date")
+	private Date date;
+	
+	@Column(name = "resultat")
+	private double resultat;
+
+}
