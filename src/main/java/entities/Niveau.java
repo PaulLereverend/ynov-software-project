24404e6357eb824ead8c60427cf6789f93b81c9b
Paul <paulereverend@gmail.com>
2020-05-05 16:35:00 +0200

    Correction des entités

--- src/main/java/entities/Niveau.java
+++ src/main/java/entities/Niveau.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,6 +13,7 @@ import javax.persistence.OneToMany;
 
 import model.Plateau;
 
+@Entity
 public class Niveau {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
